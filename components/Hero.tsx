daa504791d6a2cb5108176468627637ebc0bbc66
tariq yunusa <tariikhyunusa@gmail.com>
2023-10-09 20:48:39 +0100

    added image src to the nextConfig, constructed path for IMG display

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
                 <div>
                   <li key={movie.id}>{movie.title}</li>
                   <Image
-                    src={movie.poster_path}
+                    src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
                     alt={movie.title}
                     width={100}
                     height={100}
