ea7c7dc3f0a3dfb8b6f45e5e2e89ca44087b2209
Tariq Yunusa <tariikhyunusa@gmail.com>
2024-06-13 13:37:21 +0100

    debugged hero text animation 'including clippy'

--- app/Animations/anim.ts
+++ app/Animations/anim.ts
@@ -1,3 +1,4 @@
+"use client"
 import gsap from 'gsap';
 import { SplitText } from 'gsap-trial/SplitText';
 gsap.registerPlugin(SplitText)
@@ -6,18 +7,32 @@ gsap.registerPlugin(SplitText)
 const AnimateTitle = (element : any) => {
   let mySplitText = new SplitText('.hero_title',{type: 'words'} )
 
-   let chars = mySplitText.words
+   let words = mySplitText.words
+   gsap.set(words, { clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)' });
+
+   // Animate the text with clipPath and other properties
+   gsap.fromTo(words,
+     { yPercent: 100 },
+     {
+       yPercent: 0,
+       clipPath: 'polygon(0 0%, 100% 0%, 100% 100%, 0% 100%)',
+       stagger: 0.02,
+       ease: 'back.out',
+       delay: 1,
+       duration: 2
+     }
+   );
+
+  //  gsap.from(chars, {
+  //   yPercent: 100,
+  //   stagger: 0.02,
+  //   ease: 'back.out',
+  //   delay: 0.5,
+  //   duration: 1,
+  //   // clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)'
+
+  //  })
   
-
-   gsap.from(chars, {
-    yPercent: 100,
-    stagger: 0.02,
-    ease: 'back.out',
-    delay: 0.5,
-    duration: 1,
-    // clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)'
-
-   })
   };
   
 
